084c2d4eef975cff9cdb9595a78c313853a369df
Kevin Dorff <kevin.dorff@peoplefluent.com>
2021-03-08 12:46:16 -0600

    Updates to mani

--- mani.groovy
+++ mani.groovy
@@ -1,5 +1,9 @@
+
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
+
+import java.util.regex.Pattern
+import java.util.regex.Matcher
 
 class mani {
 
@@ -9,8 +13,9 @@ class mani {
     new mani().run(args as List);
   }
 
+  File jarsMapFile 
+  Map<String, List<String>> jarsMap = [:]
   List<String> jarFilenames = []
-  String jarFilename = null
   boolean listMode = false
   boolean includeDist = false
 
@@ -27,7 +32,7 @@ class mani {
       }
     }
 
-    if (jarFilenames.size() != 1) {
+    if (!jarFilenames.size()) {
       println "Please provide only one argument, a jar file."
       println "Can pass modes:"
       println " --list/-l       just list jar files, don't dump manifest for first"
@@ -35,20 +40,20 @@ class mani {
       println "jarFilenames? ${jarFilenames}"
       System.exit(1)
     }
-    jarFilename = jarFilenames[0]
   }
+
+  int numTopLevelJars = 0
+  int numTopLevelJarsNotFound = 0
+  int totalJarsFound = 0
 
   void run(List<String> args) {
 
     parseArgs(args)
-
     long start = 0
     long end  = 0
 
-    File jarsMapFile = new File(CACHE_FILE)
-    Map<String, List<String>> jarsMap = [:]
+    jarsMapFile = new File(CACHE_FILE)
     if (!jarsMapFile.exists()) {
-
       // The list of jars is pretty static...
       // Create a map of jars foud within the current folder
       // and below. Write the map to CACHE_FILE
@@ -57,10 +62,10 @@ class mani {
       def findJarsClosure = { File file ->
         if (file.name.endsWith(".jar")) {
             String filePath = file.path
-            if (!jarsMap[file.name.toLowerCase()]) {
-              jarsMap[file.name.toLowerCase()] = []
+            if (!jarsMap[file.name]) {
+              jarsMap[file.name] = []
             }
-            jarsMap[file.name.toLowerCase()] << file.path
+            jarsMap[file.name] << file.path
         }
       }
 
@@ -81,28 +86,67 @@ class mani {
       jarsMap = jsonSlurper.parseText(jarsMapFile.text)
     }
 
-    List<String> foundJars = jarsMap[jarFilename.toLowerCase()].findAll { jar ->
+    // Execute the searches
+    handleGlobs(jarFilenames).each { String jarFilename ->
+      iteration(jarFilename)
+    }
+
+    println "Found ${totalJarsFound} for ${numTopLevelJars} jars"
+    println "Didn't find ${numTopLevelJarsNotFound} jars"
+  }
+
+  void iteration(String jarFilename) {
+    List<String> foundJars = jarsMap[jarFilename].findAll { jar ->
       // Ignore copies in \dist\ folders unless we are running -d, --dist
       return includeDist || !jar.toLowerCase().contains(File.separator + 'dist' + File.separator)
     }
 
     if (!foundJars) {
       // The requested jar file didn't exist.
-      println "Jar ${jarFilename} not found"
-      System.exit(2)
+      println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      println "!! Jar ${jarFilename} not found"
+      println ""
+      numTopLevelJarsNotFound++
+      return
     }
 
     // Output the manifest of the jar file
-    println ""
-    println "----Manifest for${includeDist ? " (includes /dist/ jars)" : ""}"
-    foundJars.each {
-      println "---- ${it}"
-    }
-    if (!listMode) {
-      new java.util.jar.JarFile(foundJars[0]).manifest.mainAttributes.entrySet().each {
-        println "${it.key}: ${it.value}"
+    println "--------------------------------------------------------"
+    println "---- Matches for [${numTopLevelJars}] ${jarFilename} ${includeDist ? "(includes ${File.separator}dist${File.separator} jars)" : ""}"
+    foundJars.eachWithIndex { path, index ->
+      println "---- [${index}] ${path}"
+      totalJarsFound++
+      if (!listMode) {
+        try {
+          println "---- Manifest for [${index}] ${path}"
+          new java.util.jar.JarFile(foundJars[0]).manifest.mainAttributes.entrySet().each {
+            println "${it.key}: ${it.value}"
+          }
+        }
+        catch (Exception e) {
+          println "!! ERROR reading manifest"
+        }
+        println ""
       }
     }
     println ""
+    numTopLevelJars++
+  }
+
+  List<String> handleGlobs(List<String> jarFilenames) {
+    List<String> result = []
+    jarFilenames.each { String jarFilename ->
+      // Very simple glob to regex (only supporting * and fixing .)
+      String jarRegex = jarFilename.replaceAll('[.]', '\\\\.').replaceAll('\\*', '.*')
+      Pattern jarPattern = Pattern.compile(jarRegex)
+      jarsMap.keySet().each { String jarInMap ->
+        Matcher matcher = jarInMap =~ jarPattern
+        matcher.find()
+        if (matcher.size() > 0) {
+          result << jarInMap
+        }
+      }
+    }
+    return result
   }
 }
